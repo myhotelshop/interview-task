cf9083c8f25074294152855d988f8e9551603576
Mohammed Yehia <engmohammedyehia@hotmail.com>
2019-03-30 14:09:32 +0100

    Added ORM pack to the project

---
+++ src/tracking/api/v1/Entity/Customer.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\tracking\api\v1\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\tracking\api\v1\Repository\CustomerRepository")
+ */
+class Customer
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=280)
+     * @Assert\NotBlank()
+     */
+    private $name;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setText(string $name): void
+    {
+        $this->name = $name;
+    }
+}
