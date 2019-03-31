af0111193bef71a0815114b9d6fc0071d18ca30f
Mohammed Yehia <engmohammedyehia@hotmail.com>
2019-03-31 10:15:02 +0200

    * Added PlatformRevenu Model and Repository for revenue distribution

---
+++ src/tracking/api/v1/Entity/PlatformRevenue.php
@@ -0,0 +1,134 @@
+<?php
+namespace App\tracking\api\v1\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\tracking\api\v1\Repository\PlatformRepository")
+ */
+class PlatformRevenue
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Customer")
+     */
+    private $customer;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Platform")
+     */
+    private $platform;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Assert\NotBlank()
+     */
+    private $revenue;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     * @Assert\NotBlank()
+     */
+    private $created;
+
+    /**
+     * @ORM\Column(type="guid")
+     */
+    private $bookingReference;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCustomer()
+    {
+        return $this->customer;
+    }
+
+    /**
+     * @param Customer $customer
+     */
+    public function setCustomer(Customer $customer): void
+    {
+        $this->customer = $customer;
+    }
+
+    /**
+     * @return Platform
+     */
+    public function getPlatform()
+    {
+        return $this->platform;
+    }
+
+    /**
+     * @param mixed $platform
+     */
+    public function setPlatform(Platform $platform): void
+    {
+        $this->platform = $platform;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRevenue(): int
+    {
+        return (int) $this->revenue;
+    }
+
+    /**
+     * @param int $revenue
+     */
+    public function setRevenue(int $revenue): void
+    {
+        $this->revenue = $revenue;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * @param mixed $created
+     */
+    public function setCreated($created): void
+    {
+        $this->created = $created;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBookingReference()
+    {
+        return $this->bookingReference;
+    }
+
+    /**
+     * @param mixed $bookingReference
+     */
+    public function setBookingReference($bookingReference): void
+    {
+        $this->bookingReference = $bookingReference;
+    }
+}
